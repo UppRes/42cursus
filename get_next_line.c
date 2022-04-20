c98019928f68bb440f37373a3b4b7a71f09619ae
Yusuf Kuru <ykuru@k2m08s01.42kocaeli.com.tr>
2022-04-20 16:32:27 +0300

    :/

--- get_next_line.c
+++ get_next_line.c
@@ -13,10 +13,6 @@
 #include"get_next_line.h"
 #include <stdio.h>
 
-static char	*rescuedbuff;
-char	*new;
-char	buff[BUFFER_SIZE];
-
 static int	ft_strlen(char *s)
 {
 	int	i;
@@ -27,87 +23,102 @@ static int	ft_strlen(char *s)
 	return (i);
 }
 
-static char	*ft_join_up_to_chr(char *old, char *buff, int c)
+static char	*ft_join_up_to_next(char *rescuedbuff, char *old, char *buff)
 {
-	unsigned int	top_len;
 	char			*new;
+	unsigned int	top_len;
+	unsigned int	i;
 
-	if (!old || !buff)
-		return (NULL);
-	top_len = ft_strlen(old) + ft_strlen(buff);
+	i = 0;
+	// if (!old || !buff)
+	// 	return (NULL);
+	top_len = ft_strlen(rescuedbuff) + ft_strlen(old) + ft_strlen(buff) + 1;
 	new = malloc(top_len);
 	if (!new)
 		return (NULL);
+	while (*rescuedbuff)
+		*(new++) = *(rescuedbuff + i++);
 	while (*old)
 		*(new++) = *(old++);
-	while (*buff && *buff != c)
+	while (*buff && *buff != '\n')
 		*(new++) = *(buff++);
-	return (new - top_len);
-}
-
-char	*ft_rescue(char *rescuedbuff,char *buff, int c)
-{
-	unsigned int	top_len;
-	char			*new;
-
-	if (!old || !buff)
-		return (NULL);
-	top_len = ft_strlen(old) + ft_strlen(buff);
-	new = malloc(top_len);
-	if (!new)
-		return (NULL);
-	while (*old)
-		*(new++) = *(old++);
-	while (*buff && *buff != c)
-		*(new++) = *(buff++);
-	return (new - top_len);
-	while (*buff)
+	if (*buff == '\n')
 	{
-		if(*buff == '\n' && *(buff + 1))
-			return (buff + 1);
-		buff++;
+		*new = *(buff++);
+		i = 0;
+		while (*buff)
+			*(rescuedbuff + i) = *(buff++);
 	}
-	return (NULL);
+	return (new - top_len);
 }
 
-//static int	ft_is_the_next_line(char *buff)
+// char	*ft_rescue(char *rescuedbuff,char *buff, int c)
+// {
+// 	unsigned int	top_len;
+// 	char			*new;
+
+// 	if (!rescuedbuff || !buff)
+// 		return (NULL);
+// 	top_len = ft_strlen(rescuedbuff) + ft_strlen(buff);
+// 	new = malloc(top_len);
+// 	if (!new)
+// 		return (NULL);
+// 	while (*rescuedbuff)
+// 		*(new++) = *(rescuedbuff++);
+// 	while (*buff && *buff != c)
+// 		buff++;
+// 	while ()
+// 	{
+// 		/* code */
+// 	}
+
+// 	return (new - top_len);
+// 	while (*buff)
+// 	{
+// 		if(*buff == '\n' && *(buff + 1))
+// 			return (buff + 1);
+// 		buff++;
+// 	}
+// 	return (NULL);
+// }
+
+static int	ft_is_the_next_line(char *buff)
 {
 	int	i;
 
 	i = 0;
-	while (i < BUFFER_SIZE)
+	while (*buff && *buff != '\n')
+		buff++;
+	if (*(buff++) == '\n')
 	{
-		while (buff[i] == '\n' && i < BUFFER_SIZE)
-		{
-			rescuedbuff
-		}
-		else
-		{
-		}
+		while (buff[i])
+			i++;
 	}
-	return (new);
+	return (i);
 }
 
 char	*get_next_line(int fd)
 {
+	char		buff[BUFFER_SIZE];
+	char		*new;
+	static char	*rescuedbuff;
 
 	new = malloc(0);
-	new = ft_join_up_to_chr(new, rescuedbuff, '\0');
 	read(fd, buff, BUFFER_SIZE);
-	while (ft_is_the_next_line(buff))
+	while (0 == ft_is_the_next_line(buff))
 	{
-		new = ft_join_up_to_chr(new, buff, '\0');
+		new = ft_join_up_to_next(rescuedbuff, new, buff);
+		rescuedbuff = malloc(ft_is_the_next_line(buff));
 		read(fd, buff, BUFFER_SIZE);
+		free(rescuedbuff);
 	}
-	new = ft_join_up_to_chr(new, buff, '\n');
-	rescuedbuff = ft_rescue(rescuedbuff, buff, '\0');
 	return (new);
 }
 
 int	main(void)
 {
 	int	fd;
-	
+
 	fd = open("text", O_RDONLY);
 	printf("%s\n", get_next_line(fd));
 	printf("%s\n", get_next_line(fd));
